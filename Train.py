6c7a694b1e701d26fe25be52fcbbec64ced0ee8a
conda act pt <vscode@163.com>
2020-08-23 10:48:14 +0800

    add save each epoch model

--- Train.py
+++ Train.py
@@ -1,19 +1,19 @@
-print('Setting Up')
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
-from Utils import *
 from sklearn.model_selection import train_test_split
+from Utils import *
+import os
+print('Setting Up')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 
 path = '/Users/sangyy/Documents/beta_simulator_mac/dataset'
 data = importData(path)
 
-data = balancedData(data, display=False)
+data = balancedData(data, display=True)
 
 imagesPath, steerings = loadData(path, data)
 
-xTrain, xVal, yTrain,yVal = train_test_split(imagesPath,steerings, test_size=0.2,random_state=5)
+xTrain, xVal, yTrain, yVal = train_test_split(
+    imagesPath, steerings, test_size=0.2, random_state=5)
 print('Total Training Images: ', len(xTrain))
 print('Total Validation Images: ', len(xVal))
 
@@ -21,17 +21,18 @@ model = createModel()
 model.summary()
 
 filepath = 'model-ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5'
-checkpoint = ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-history = model.fit(batchGen(xTrain,yTrain,100,1),steps_per_epoch=300,epochs=10,callbacks=[checkpoint],
-           validation_data=batchGen(xVal, yVal,100,0), validation_steps=200)
+checkpoint = ModelCheckpoint(
+    filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+history = model.fit(batchGen(xTrain, yTrain, 100, 1), steps_per_epoch=300, epochs=10, callbacks=[checkpoint],
+                    validation_data=batchGen(xVal, yVal, 100, 0), validation_steps=200)
 
-# model.save('model.h5')
-# print('Model Saved')
+model.save('model.h5')
+print('End!!! ALL Model Saved')
 
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
-plt.legend(['Training' , 'Validation'])
-plt.ylim([0,1])
+plt.legend(['Training', 'Validation'])
+plt.ylim([0, 1])
 plt.title('Loss')
 plt.xlabel('Epoch')
 plt.show()
--- Utils.py
+++ Utils.py
@@ -1,20 +1,16 @@
-import os 
-import random 
+import os
+import random
 import cv2
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from imgaug import augmenters as iaa
 from sklearn.utils import shuffle
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Conv2D,Flatten  
+from tensorflow.keras.layers import Dense, Conv2D, Flatten
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint
-
-
-
-
 
 
 ##### Name #####
@@ -22,58 +18,57 @@ def getName(filepath):
     return filepath.split('\\')[-1]
 
 
-
 ##### Data Importation #####
 def importData(path):
-    columns = ['Center', 'Left', 'Right', 'Steering', 'Throttle', 'Brake', 'Speed']
-    data = pd.read_csv(os.path.join(path,'driving_log.csv'),names=columns)
+    columns = ['Center', 'Left', 'Right',
+               'Steering', 'Throttle', 'Brake', 'Speed']
+    data = pd.read_csv(os.path.join(path, 'driving_log.csv'), names=columns)
     data['Center'] = data['Center'].apply(getName)
     print('Total Images Imported:', data.shape[0])
     return data
 
 
-
-##### Balance the Data ##### 
-def balancedData(data,display=True):
+##### Balance the Data #####
+def balancedData(data, display=True):
     nBins = 31
     samplesPerBin = 1000
-    hist, bins = np.histogram(data['Steering'],nBins)
-    
+    # print(data['Steering'])
+    hist, bins = np.histogram(data['Steering'], nBins)
+
     if display:
         center = (bins[:-1] + bins[1:])*0.5
         plt.bar(center, hist, width=0.06)
-        plt.plot((-1,1),(samplesPerBin,samplesPerBin))
+        plt.plot((-1, 1), (samplesPerBin, samplesPerBin))
         plt.show()
-    
+
     removeIndexList = []
     for j in range(nBins):
         binDataList = []
         for i in range(len(data['Steering'])):
             if data['Steering'][i] >= bins[j] and data['Steering'][i] <= bins[j+1]:
                 binDataList.append(i)
-        binDataList  = shuffle(binDataList)
+        binDataList = shuffle(binDataList)
         binDataList = binDataList[samplesPerBin:]
         removeIndexList.extend(binDataList)
     print('Removed Images: ', len(removeIndexList))
     data.drop(data.index[removeIndexList], inplace=True)
     print('Remaining Images: ', len(data))
-    
+
     if display:
         hist, _ = np.histogram(data['Steering'], nBins)
         plt.bar(center, hist, width=0.06)
-        plt.plot((-1,1),(samplesPerBin, samplesPerBin))
+        plt.plot((-1, 1), (samplesPerBin, samplesPerBin))
         plt.show()
-        
-    return data
 
+    return data
 
 
 ##### Load the Data #####
 def loadData(path, data):
     imagesPath = []
     steering = []
-    
-    for i in range (len(data)):
+
+    for i in range(len(data)):
         indexedData = data.iloc[i]
         imagesPath.append(os.path.join(path, 'IMG', indexedData[0]))
         steering.append(float(indexedData[3]))
@@ -82,50 +77,47 @@ def loadData(path, data):
     return imagesPath, steering
 
 
-
 ##### Image Augmentation #####
 def augmentImage(imgPath, steering):
     img = mpimg.imread(imgPath)
 
-   
-    ### PAN
+    # PAN
     if np.random.rand() < 0.5:
-        pan = iaa.Affine(translate_percent={'x':(-0.1, 0.1), 'y':(-0.1, 0.1)}) 
+        pan = iaa.Affine(translate_percent={
+                         'x': (-0.1, 0.1), 'y': (-0.1, 0.1)})
         img = pan.augment_image(img)
-        
-    ### ZOOM
+
+    # ZOOM
     if np.random.rand() < 0.5:
         zoom = iaa.Affine(scale=(1, 1.2))
         img = zoom.augment_image(img)
-    
-    ### BRIGHTNESS
-    if np.random.rand() < 0.5:
-        brightness = iaa.Multiply((0.3,1.2))
-        img = brightness.augment_image(img)
-    
-    ### FLIP
-    if np.random.rand() < 0.5:
-        img = cv2.flip(img,1)
-        steering = -steering
-    
-    
-    return img, steering
 
+    # BRIGHTNESS
+    if np.random.rand() < 0.5:
+        brightness = iaa.Multiply((0.3, 1.2))
+        img = brightness.augment_image(img)
+
+    # FLIP
+    if np.random.rand() < 0.5:
+        img = cv2.flip(img, 1)
+        steering = -steering
+
+    return img, steering
 
 
 ##### Preprocessing the Image #####
 def preProcessing(img):
-    img = img[60:135,:,:]
+    img = img[60:135, :, :]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    img = cv2.GaussianBlur(img,(3,3),0)
-    img = cv2.resize(img,(200,66))
-    img = img /255
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+    img = cv2.resize(img, (200, 66))
+    img = img / 255
     return img
+
 
 imgRe = preProcessing(mpimg.imread('test.jpg'))
 plt.imshow(imgRe)
 plt.show()
-
 
 
 ##### Generating Batch #####
@@ -133,11 +125,12 @@ def batchGen(imagesPath, steeringList, batchSize, trainFlag):
     while True:
         imgBatch = []
         steeringBatch = []
-        
+
         for i in range(batchSize):
             index = random.randint(0, len(imagesPath)-1)
             if trainFlag:
-                img, steering = augmentImage(imagesPath[index], steeringList[index])
+                img, steering = augmentImage(
+                    imagesPath[index], steeringList[index])
             else:
                 img = mpimg.imread(imagesPath[index])
                 steering = steeringList[index]
@@ -145,28 +138,26 @@ def batchGen(imagesPath, steeringList, batchSize, trainFlag):
             img = preProcessing(img)
             imgBatch.append(img)
             steeringBatch.append(steering)
-        yield(np.asarray(imgBatch),np.asarray(steeringBatch))
- 
- 
- 
-##### Model Creation #####        
+        yield(np.asarray(imgBatch), np.asarray(steeringBatch))
+
+
+##### Model Creation #####
 def createModel():
     model = Sequential()
-    
-    model.add(Conv2D(24,(5,5),(2,2), input_shape=(66, 200, 3), activation='elu'))
-    model.add(Conv2D(36,(5,5),(2,2), activation='elu'))
-    model.add(Conv2D(48,(5,5),(2,2), activation='elu'))
-    model.add(Conv2D(64,(3,3), activation='elu'))
-    model.add(Conv2D(64,(3,3), activation='elu'))
-    
+
+    model.add(Conv2D(24, (5, 5), (2, 2), input_shape=(
+        66, 200, 3), activation='elu'))
+    model.add(Conv2D(36, (5, 5), (2, 2), activation='elu'))
+    model.add(Conv2D(48, (5, 5), (2, 2), activation='elu'))
+    model.add(Conv2D(64, (3, 3), activation='elu'))
+    model.add(Conv2D(64, (3, 3), activation='elu'))
+
     model.add(Flatten())
-    model.add(Dense(100,activation='elu'))
-    model.add(Dense(50,activation='elu'))
-    model.add(Dense(10,activation='elu'))
+    model.add(Dense(100, activation='elu'))
+    model.add(Dense(50, activation='elu'))
+    model.add(Dense(10, activation='elu'))
     model.add(Dense(1))
-    
-    model.compile(Adam(learning_rate=0.0001),loss='MSE')
-    
+
+    model.compile(Adam(learning_rate=0.0001), loss='MSE')
+
     return model
-    
-    
